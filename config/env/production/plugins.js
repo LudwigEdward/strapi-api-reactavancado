bcaf7c34f08dbe6104b62383e8a12ee6d2144bfb
Luís Eduardo <le.barros20166@hotmail.com>
2020-08-03 09:06:23 -0400

    fix: removing trash

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "aws-s3",
+    provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION"),
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: 'aws-region',
       params: {
-        Bucket: env("AWS_BUCKET"),
+        Bucket: 'my-bucket',
       },
     },
   },
